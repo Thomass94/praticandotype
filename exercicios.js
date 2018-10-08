52be7dcf6e6537d0d99df63f163e700373e24399
thomass94 <thomassilvamedeiros@gmail.com>
2018-10-08 16:56:56 -0300

    exercicios.js

--- exercicios.js
+++ exercicios.js
@@ -1,6 +1,29 @@
-//exercicio
+//exercicio return name
 function nome() {
     return "Thomas";
 }
 var user = "Thomas";
 console.log("Thomas");
+//exercicio boolean
+var qualquer = true;
+console.log(true);
+//exercicio parametros e valores
+var calculadora = /** @class */ (function () {
+    function calculadora() {
+    }
+    calculadora.prototype.Calculadora = function () { };
+    calculadora.prototype.somar = function () {
+        var result = 4 + 5;
+        return result;
+    };
+    calculadora.prototype.subtração = function () {
+        var result = 7 - 5;
+        return result;
+    };
+    return calculadora;
+}());
+var calc = new calculadora;
+var soma = calc.somar();
+var subtração = calc.subtração();
+console.log;
+console.log(subtração);
